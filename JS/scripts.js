026ebd6d1841797e71ef5d4f956b623ba62e5105
megagig <megagigsoftwaresolutions@gmail.com>
2023-10-10 16:36:24 +0100

    Set default style

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,6 +8,12 @@ const clock = () => {
   ctx.clearRect(0, 0, 500, 500); //clear the canvas
   ctx.translate(250, 250); //put 0,0 on the center of the canvas
   ctx.rotate(-Math.PI / 2); //rotate -90 degrees
+
+  //set default styles
+  ctx.strokeStyle = '000000';
+  ctx.fillStyle = 'f4f4f4';
+  ctx.lineWidth = 5;
+  ctx.lineCcap = 'round';
 };
 
 clock();
